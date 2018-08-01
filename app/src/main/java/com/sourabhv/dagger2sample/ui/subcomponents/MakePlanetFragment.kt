c841c1b7515ed68451cb14ecced1bed42fe335dd
Sourabh Verma <sourabh.coder@gmail.com>
2018-08-02 01:14:18 +0530

    Make adapter injectable

--- app/src/main/java/com/sourabhv/dagger2sample/ui/subcomponents/MakePlanetFragment.kt
+++ app/src/main/java/com/sourabhv/dagger2sample/ui/subcomponents/MakePlanetFragment.kt
@@ -3,7 +3,6 @@ package com.sourabhv.dagger2sample.ui.subcomponents
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,6 @@ import dagger.android.support.DaggerFragment
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.fragment_make_planet.*
-import kotlinx.android.synthetic.main.li_item.view.*
 import javax.inject.Inject
 
 // This scope defines the lifecycle for Fragment
@@ -23,7 +21,7 @@ import javax.inject.Inject
 class MakePlanetFragment @Inject constructor() : DaggerFragment() {
 
     @Inject @CartID lateinit var cartID: String
-    lateinit var makePlanetItemsAdapter: MakePlanetItemsAdapter
+    @Inject lateinit var makePlanetItemsAdapter: MakePlanetItemsAdapter
     @Inject lateinit var makePlanetRepository: MakePlanetRepository
     private var disposable: Disposable? = null
 
@@ -36,7 +34,6 @@ class MakePlanetFragment @Inject constructor() : DaggerFragment() {
         super.onViewCreated(view, savedInstanceState)
         tvCartId.text = "Card: $cartID"
         rvItems.layoutManager = LinearLayoutManager(context)
-        makePlanetItemsAdapter = MakePlanetItemsAdapter(makePlanetRepository)
         rvItems.adapter = makePlanetItemsAdapter
     }
 
@@ -52,45 +49,5 @@ class MakePlanetFragment @Inject constructor() : DaggerFragment() {
         super.onStop()
     }
 
-
-    class MakePlanetItemsAdapter constructor(
-            private val makePlanetRepository: MakePlanetRepository
-    ) : RecyclerView.Adapter<MakePlanetItemsAdapter.ItemHolder>() {
-
-        var items: List<Item> = listOf()
-            set(value) {
-                field = value
-                notifyDataSetChanged()
-            }
-
-        override fun getItemCount() = items.size
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            return ItemHolder(inflater.inflate(R.layout.li_item, parent, false))
-        }
-
-        override fun onBindViewHolder(holder: ItemHolder, position: Int) {
-            holder.bind()
-        }
-
-        inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-            init {
-                itemView.tvAdd.setOnClickListener {
-                    val item = items[adapterPosition]
-                    makePlanetRepository.updateItem(item.copy(quantity = item.quantity + 1))
-                }
-            }
-
-            @SuppressLint("SetTextI18n")
-            fun bind() {
-                val item = items[adapterPosition]
-                itemView.tvName.text = "${item.quantity} x ${item.name}"
-                itemView.tvAmount.text = item.amount.toString()
-            }
-        }
-
-    }
 
 }
